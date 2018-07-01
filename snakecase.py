f0993ab2f89f231a5917bbfe85e85c4f3c797c74
MaGiCMaN <maglc_man@hotmail.com>
2018-07-01 17:44:22 -0500

    Complete and clean up code

--- snakecase.py
+++ snakecase.py
@@ -1,22 +1,39 @@
 def to_underscore(string):
     s = string
-    the_len = len(string)
+    if s == str(s):
+        the_len = len(string)
 
-    for i in string:
-        x = string.index(i)-1
-        num = x % len(string)
-        if string[x+1].upper() == i:
-            if num + 1 != len(string):
-                try:
-                    z = int(i)
-                    z += 0
-                    n = num + 2
-                    s = s[:n] + "_" + s[n:]
-                except ValueError:
-                    s = s[:s.index(i)] + "_" + s[s.index(i):]
+        for i in string:
+            x = string.index(i) - 1
+            num = x % len(string)
+            if string[x + 1].upper() == i:
+                if num + 1 != len(string):
+                    try:
+                        z = int(i)
+                        z += 0
+                        n = num + 2
+                        s = s[:n] + "_" + s[n:]
+                    except ValueError:
+                        s = s[:s.index(i)] + "_" + s[s.index(i):]
 
+        s = s.lower()
+        s = list(s)
+        for i in s:
+            if i == "_":
+                indx = s.index(i) + 1
+                s[s.index(i)] = "*"
+                if s[indx] == "_":
+                    del s[indx]
 
-    s = s.lower()
-    return s
+        for i in s:
+            if i == "*":
+                s[s.index(i)] = "_"
+
+        s = ''.join(s)
+
+    else:
+        print("Argument cannot be only a number")
+
+    return str(s)
 
 to_underscore('TestController')
