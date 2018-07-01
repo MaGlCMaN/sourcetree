8815e7e68259234dec0fe926bd5c919fd5a36f16
MaGiCMaN <maglc_man@hotmail.com>
2018-07-01 16:42:42 -0500

    Add underscore functionality

--- snakecase.py
+++ snakecase.py
@@ -1,6 +1,15 @@
 def to_underscore(string):
-    string = string.lower()
-    print(string)
+    s = string
+    the_len = len(string)
 
+    for i in string:
+        x = string.index(i)-1
+        num = x % len(string)
+        if string[x+1].upper() == i:
+            if num + 1 != len(string):
+                s = s[:s.index(i)] + "_" + s[s.index(i):]
+
+
+    string = string.lower()
 
 to_underscore('TestController')
